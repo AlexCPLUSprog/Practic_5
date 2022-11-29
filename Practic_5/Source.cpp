3b83d57e053531df5950bbf3949de0bee12395fb
Алекс <Alexandr_enid@mail.ru>
2022-11-29 19:49:56 +0500

    циферки поменял

--- Practic_5/Source.cpp
+++ Practic_5/Source.cpp
@@ -16,11 +16,11 @@ int main() {
 		std::cout << "Доступные команды:\n1. Вывод информации о книге.\n2. Показать автора книги.\n3. Записать информацию о книге в указанный путь.\n4. Добавить книгу в конец массива.\n5. Очистить массив.\n6. Завершение работы.\nВведите номер команды -> ";
 		std::cin >> m;
 		switch (m) {
-		case 1: std::cout << "\nВведите номер книги(0 - 2) -> "; std::cin >> l; show_book(arr_book[l]);
+		case 1: std::cout << "\nВведите номер книги(1 - 3) -> "; std::cin >> l; show_book(arr_book[l - 1]);
 			continue;
-		case 2: std::cout << "\nВведите номер книги(0 - 2), чтобы увидеть фамилию автора -> "; std::cin >> l; std::cout << "Автор " << l + 1 << " книги: " << author_name(arr_book[l]) << "\n\n";
+		case 2: std::cout << "\nВведите номер книги(1 - 3), чтобы увидеть фамилию автора -> "; std::cin >> l; std::cout << "Автор " << l + 1 << " книги: " << author_name(arr_book[l - 1]) << "\n\n";
 			continue;
-		case 3: std::cout << "\nВведите номер книги(0 - 2), чтобы информацию о ней можно было записать в файл -> "; std::cin >> l; save_book(arr_book[l], "file.txt"); std::cout << "Книга успешно записана в файл.\n\n";
+		case 3: std::cout << "\nВведите номер книги(1 - 3), чтобы информацию о ней можно было записать в файл -> "; std::cin >> l; save_book(arr_book[l - 1], "file.txt"); std::cout << "Книга успешно записана в файл.\n\n";
 			continue;
 		case 4: add_book(arr_book, n); continue;
 		case 5: clear_arr(arr_book, n); std::cout << "\n";	continue;
